e65e0eb65936892384b314f48401c84a8c8f69ae
wafaagomaa <wafaagomaa2025@gmail.com>
2026-09-09 23:48:29 +0300

    Initial commit

---
+++ story_content/68jn6SsPD3O_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":79,"text":"دي الوقت جاه وقت المرحلة الثالثة،"},{"start":2359,"text":"وهي تطوير الحلول الممكنة،"},{"start":4960,"text":"في المرحلة دي"},{"start":6079,"text":"هننشغل خيالنا"},{"start":7559,"text":"ونطلع أكبر عدد ممكن من الأفكار والحلول من"},{"start":11279,"text":"غير ما نحكم عليها أو نستبعدها في البداية،"},{"start":15119,"text":"يمكن فكرة بسيطة"},{"start":16479,"text":"تبقى هي أفضل حل"},{"start":18399,"text":"يلا بينا نبدع."}]}]};
+    window.globalLoadJsAsset('story_content/68jn6SsPD3O_transcripts.js', JSON.stringify(data));
+})();
